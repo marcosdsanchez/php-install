6de9e700f960c9e7df4f66ed8ac19a85fd4a023f
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2013-10-30 16:24:31 -0300

    forgot homebrew sha

--- homebrew/php-install.rb
+++ homebrew/php-install.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PHPInstall < Formula
   homepage 'https://github.com/marcosdsanchez/php-install#readme'
   url 'https://github.com/marcosdsanchez/php-install/archive/v0.0.1.tar.gz'
-  sha1 ''
+  sha1 '7306700d7963e24bb670b37e1de5613483df24f4'
 
   head 'https://github.com/marcosdsanchez/php-install.git'
 
