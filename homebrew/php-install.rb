f5e6423f94543940124d8de37099f3653df63f4b
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2013-10-30 17:00:46 -0300

    Fix homebrew recipe

--- homebrew/php-install.rb
+++ homebrew/php-install.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class PHPInstall < Formula
+class PhpInstall < Formula
   homepage 'https://github.com/marcosdsanchez/php-install#readme'
   url 'https://github.com/marcosdsanchez/php-install/archive/v0.0.1.tar.gz'
   sha1 '7306700d7963e24bb670b37e1de5613483df24f4'
