a4d2267ee94902221fd6dbab04318e9914989d88
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2014-01-19 13:04:59 -0300

    bump homebrew recipe

--- homebrew/php-install.rb
+++ homebrew/php-install.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PhpInstall < Formula
   homepage 'https://github.com/marcosdsanchez/php-install#readme'
   url 'https://github.com/marcosdsanchez/php-install/archive/v0.0.1.tar.gz'
-  sha1 '7306700d7963e24bb670b37e1de5613483df24f4'
+  sha1 '828736801c9c5759ae973b60c7bb5e6fcfacd0e5'
 
   head 'https://github.com/marcosdsanchez/php-install.git'
 
